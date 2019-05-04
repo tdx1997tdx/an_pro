f13970498ca3c1c16688e83861bc71355a745658
tdx1997tdx <798637048@qq.com>
2019-05-04 16:55:30 +0800

    u

--- basic_mail_op/mail_server.py
+++ basic_mail_op/mail_server.py
@@ -1,5 +1,7 @@
 import smtplib
 import random
+from email.mime.text import MIMEText
+from email.header import Header
 from basic_mail_op import temp_storage as ts
 pass1='qimddlwbnympbfai'
 pass2='pehiokuarlmibdcd'
@@ -9,9 +11,15 @@ def send_email(name,receiver):
     smtp.connect('smtp.qq.com',25)
     print('连接成功')
     smtp.login('798637048@qq.com', pass2)
-    message='Note Your Life的验证码为:'+ str(v_code)
+    msg = 'Note Your Life的验证码为:' + str(v_code)
+    message = MIMEText(msg, 'plain', 'utf-8')
+    message['From'] = Header("Note Your Life的开发团队", 'utf-8')  # 发送者
+    message['To'] = Header("尊敬的玩家", 'utf-8')  # 接收者
+    subject = 'Note Your Life的验证码'
+    message['Subject'] = Header(subject, 'utf-8')
+
     try:
-        smtp.sendmail('798637048@qq.com', receiver, message.encode('utf-8'))
+        smtp.sendmail('798637048@qq.com', receiver, message.as_string())
         print('ok')
     except:
         return False
--- testing/email_test.py
+++ testing/email_test.py
@@ -1,4 +1,6 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 pass1='qimddlwbnympbfai'
 pass2='pehiokuarlmibdcd'
 smtp = smtplib.SMTP()
@@ -6,5 +8,13 @@ smtp.connect('smtp.qq.com',25)
 print('连接成功')
 smtp.login('798637048@qq.com', pass2)
 print('登陆成功')
-smtp.sendmail('798637048@qq.com', '348398625@qq.com','你好牛逼：2333333'.encode('utf-8'))
+# 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
+message = MIMEText('Note Your Life的验证码为:5555', 'plain', 'utf-8')
+message['From'] = Header("Note Your Life的开发团队", 'utf-8')  # 发送者
+message['To'] = Header("尊敬的玩家", 'utf-8')  # 接收者
+subject = 'Note Your Life的验证码'
+message['Subject'] = Header(subject, 'utf-8')
+#message='Note Your Life的验证码为:'+ str(5555)
+smtp.sendmail('798637048@qq.com', '348398625@qq.com', message.as_string())
+print('发送成功')
 smtp.quit()
