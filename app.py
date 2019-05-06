0aaeee8ac2e92da02acef84793cd9df2895aeecd
tdx1997tdx <798637048@qq.com>
2019-05-06 16:54:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, request
 from route.welcome import welcome_page
 from route.homepage import homepage_page
-from basic_mail_op import send_mail_op as smo
 app = Flask(__name__)
 app.register_blueprint(welcome_page)
 app.register_blueprint(homepage_page)
-smo.connect_to_mail_server()
 
 @app.route('/',methods=['GET','POST'])
 def hello_world():
