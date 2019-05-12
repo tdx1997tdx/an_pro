944cd8cc1404224b658ce3c86d325902f896d1a7
tdx1997tdx <798637048@qq.com>
2019-05-12 20:51:23 +0800

    u

--- basic_mail_op/op_storage.py
+++ basic_mail_op/op_storage.py
@@ -7,7 +7,7 @@ class Storage:
         self.temp_storage[name]=v_code
 
     def verification(self,name,v_code):
-        return self.temp_storage.get(name) and self.temp_storage[name]==v_code
+        return self.temp_storage.get(name) and self.temp_storage.get(name)==v_code
 
     def remove(self,name):
         self.temp_storage.pop(name)
