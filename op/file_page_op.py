a7f0a81362583ecc91052732f54ae9d81f84ec29
tdx1997tdx <798637048@qq.com>
2019-05-24 02:16:25 +0800

    u

--- op/file_page_op.py
+++ op/file_page_op.py
@@ -16,7 +16,9 @@ def file_upload_op(name,filename,content):
     sql = "select file_id from file where username=%s and filename=%s"
     para = [name,filename]
     result = [i for i in conn.select(sql, para)]
+    print(name,filename,content)
     if result==[]:
+
         insert_sql = 'insert into file (filename,username,content) values (%s,%s,%s)'
         insert_para=[filename,name,content]
         is_success = conn.iur(insert_sql, insert_para)
--- testing/request_test.py
+++ testing/request_test.py
@@ -1,4 +1,5 @@
 import requests, json
+import time,random,hashlib
 
 
 def regist():
@@ -30,7 +31,60 @@ def file_test3():
     r = requests.post(url, data)
     print(r.text)
 
+
+def py1():
+    url = 'http://fanyi.baidu.com/v2transapi'
+    data = {
+        'query': '人生如戏',
+        'from': 'zh',
+        'to': 'en',
+        'transtype': 'translang',
+
+    }
+
+    headers = {
+        'Accept-Language': 'zh - CN, zh;q = 0.9',
+        'Content-Type': 'application / x - www - form - urlencoded;charset = UTF - 8',
+        'Host': 'fanyi.baidu.com',
+        'Origin': 'https: // fanyi.baidu.com',
+        "Referer": "http://fanyi.baidu.com/",
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    response = requests.post(url, data, headers=headers)
+    print(response.content.decode("utf-8"))
+
+
+def py2():
+    url = 'http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
+    data = {
+        'i': '燃烧',
+        'from': 'AUTO',
+        'to': 'AUTO',
+        'smartresult': 'dict',
+        'client': 'fanyideskweb',
+        'doctype': 'json',
+        'version': '2.1',
+        'keyfrom': 'fanyi.web',
+        'action': 'FY_BY_REALTlME'
+    }
+    data['typoResult'] = 'false'
+    u = 'fanyideskweb'
+    f = str(int(time.time() * 1000) + random.randint(1, 10))
+    c = "ebSeFb%=XZ%T[KZ)c(sy!"
+    sign = hashlib.md5((u + '燃烧' + f + c).encode('utf-8')).hexdigest()
+    data['salt'] = f
+    data['sign'] = sign
+
+    headers = {
+        'Content-Type': 'application / x - www - form - urlencoded;charset = UTF - 8',
+        'Host': 'fanyi.youdao.com',
+        'Origin': 'http: // fanyi.youdao.com',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
+    }
+    response = requests.post(url, data, headers=headers)
+    print(response.content.decode("utf-8"))
+
 v_code=7193
 #regist()
 #regist_verify(v_code)
-file_test3()
+#file_test3()
+py2()
