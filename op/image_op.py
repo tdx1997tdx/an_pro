4ee3b9b824d2067e6e9afc85ef19c3c0a2012060
tdx1997tdx <798637048@qq.com>
2019-05-25 02:53:49 +0800

    Update image_op.py

--- op/image_op.py
+++ op/image_op.py
@@ -4,6 +4,7 @@ from basic_file_op import op_image as img
 from PIL import Image
 
 def image_translation_op(name,image):
+    print(image)
     #decode_image=base64.b64decode(image)
     path=img.dir_path+name
     if(not os.path.exists(img.dir_path)):
