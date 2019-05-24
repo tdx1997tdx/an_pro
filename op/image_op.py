c490c5a3b2d6c5df7341ac0a58e06fe9a207ce2f
tdx1997tdx <798637048@qq.com>
2019-05-25 03:02:26 +0800

    Update image_op.py

--- op/image_op.py
+++ op/image_op.py
@@ -9,6 +9,8 @@ def image_translation_op(name,image):
     path=img.dir_path+name
     if(not os.path.exists(img.dir_path)):
         os.makedirs(img.dir_path)
+    elif(os.path.exists(path)):
+        os.remove(path)
     file = open(path, 'wb')
     file.write(decode_image)
     file.close()
